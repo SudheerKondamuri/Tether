e044e303f43b7c9df0de54cb1ccf1eccc889f3dd
Sudheer <sudheersudheer7654321@gmail.com>
2026-05-31 16:48:37 +0530

    feat: complete build.gradle.kts functionality

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,3 +10,15 @@ val newBuildDir: Directory =
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
