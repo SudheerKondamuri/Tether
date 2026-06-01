a7a46092d284618b58853154bd2a63f8e165fe22
Sudheer <sudheersudheer7654321@gmail.com>
2026-06-01 09:38:36 +0530

    feat: complete settings.gradle.kts functionality

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,5 +11,16 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
-}}
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+}
+
+include(":app")
