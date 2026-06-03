8a38986bf9dc25141c6fefc6ea9762ce06fedb13
Sudheer <sudheersudheer7654321@gmail.com>
2026-06-03 15:51:53 +0530

    feat: implement database database_provider specifications

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -2,3 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tether/core/database/app_database.dart';
 
 /// Singleton database instance provider.
+final databaseProvider = Provider<AppDatabase>((ref) {
+  final db = AppDatabase();
+  ref.onDispose(() => db.close());
+  return db;
+});
