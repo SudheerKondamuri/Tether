99e88b5622665efd49b18af292729366c9fadf89
Sudheer <sudheersudheer7654321@gmail.com>
2026-06-05 11:15:40 +0530

    feat: bootstrap MainActivity.kt scaffolding

---
+++ android/app/src/main/kotlin/com/tether/app/tether/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tether.app.tether
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    private var clipboardPlugin: ClipboardPlugin? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        // Register clipboard plugin
+        clipboardPlugin = ClipboardPlugin(this)
+        clipboardPlugin?.register(flutterEngine)
+
+}}
