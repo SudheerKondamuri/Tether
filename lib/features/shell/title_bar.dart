5363c8a01a43766c14c4651e6b98a66791be36b3
Sudheer <sudheersudheer7654321@gmail.com>
2026-06-06 13:44:13 +0530

    feat: wire up UI handlers and state for title_bar

--- lib/features/shell/title_bar.dart
+++ lib/features/shell/title_bar.dart
@@ -62,5 +62,67 @@ class TitleBar extends StatelessWidget {
             icon: Icons.remove,
             onTap: () {},
           ),
+          const SizedBox(width: 4),
+          _WindowButton(
+            icon: Icons.crop_square,
+            onTap: () {},
+          ),
+          const SizedBox(width: 4),
+          _WindowButton(
+            icon: Icons.close,
+            onTap: () {},
+            hoverColor: TetherColors.accentDanger,
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-}}
+class _WindowButton extends StatefulWidget {
+  final IconData icon;
+  final VoidCallback onTap;
+  final Color? hoverColor;
+
+  const _WindowButton({
+    required this.icon,
+    required this.onTap,
+    this.hoverColor,
+  });
+
+  @override
+  State<_WindowButton> createState() => _WindowButtonState();
+}
+
+class _WindowButtonState extends State<_WindowButton> {
+  bool _hovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovering = true),
+      onExit: (_) => setState(() => _hovering = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Container(
+          width: 28,
+          height: 28,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: _hovering
+                ? (widget.hoverColor?.withAlpha(40) ??
+                    TetherColors.surfaceHigher)
+                : Colors.transparent,
+          ),
+          child: Icon(
+            widget.icon,
+            size: 16,
+            color: _hovering
+                ? (widget.hoverColor ?? TetherColors.textPrimary)
+                : TetherColors.textSecondary,
+          ),
+        ),
+      ),
+    );
+  }
+}
