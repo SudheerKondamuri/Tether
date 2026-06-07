676d08965fb4c47a8f5a7000500b6d5a111bae38
Sudheer <sudheersudheer7654321@gmail.com>
2026-06-07 17:03:49 +0530

    feat: complete widget_test.dart functionality

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,5 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tether/main.dart';
 
 void main() {
-
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: TetherApp()));
+    expect(find.byType(TetherApp), findsOneWidget);
+  });
 }
