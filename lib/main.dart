41c6b87932ae81f91bb66d3ebb43aa6bc19c680f
Sudheer <sudheersudheer7654321@gmail.com>
2026-06-07 15:33:59 +0530

    feat: bootstrap Flutter main application entrypoint

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tether/shared/theme.dart';
+import 'package:tether/shared/platform_utils.dart';
+import 'package:tether/features/shell/linux_shell.dart';
+import 'package:tether/features/shell/android_shell.dart';
+import 'package:tether/core/services/clipboard_service.dart';
+import 'package:tether/core/services/notification_bridge_service.dart';
+import 'package:tether/core/services/file_service.dart';
+import 'package:tether/core/networking/connection_manager.dart';
+import 'package:tether/core/networking/mdns_discovery.dart';
+import 'package:tether/shared/constants.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: TetherApp()));
+}
+
+class TetherApp extends ConsumerStatefulWidget {
+  const TetherApp({super.key});
+
+  @override
+  ConsumerState<TetherApp> createState() => _TetherAppState();
+}
+
+class _TetherAppState extends ConsumerState<TetherApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Schedule service startup after first frame
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _initServices();
+    });
+  }
+
+  Future<void> _initServices() async {
+    // Start connection manager (TCP server)
+    final connectionManager = ref.read(connectionManagerProvider);
+
+}}
